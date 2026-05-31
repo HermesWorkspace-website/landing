eca9fb2d7b6886f57f96d09f9871fa5864ef3917
apurav-invisible <sonuagarwal.chandwa@gmail.com>
2026-05-31 19:00:53 +0530

    Update next.config.mjs image qualities

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     allowedDevOrigins: ["pointing-factor-monorail.ngrok-free.dev"],
+    images: {
+        qualities: [75, 92]
+    }
 };
 
 export default nextConfig;

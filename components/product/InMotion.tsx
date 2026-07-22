ee85bc41afe05d93bad2c15fa2802ec2f1e44e3f
Lakshya kumar <73013235+Lakshyakumar266@users.noreply.github.com>
2026-07-23 00:43:31 +0530

    Update video source URL in InMotion component

--- components/product/InMotion.tsx
+++ components/product/InMotion.tsx
@@ -90,7 +90,7 @@ export default function InMotion() {
                 onEnded={() => setPlaying(false)}
                 onPause={() => setPlaying(false)}
                 onPlay={() => setPlaying(true)}
-                src="https://ik.imagekit.io/hermesworkspace/Landing/assets/hermes-launch.mp4"
+                src="https://ik.imagekit.io/hermesworkspace/Landing/assets/product_launch.mp4"
               >
                 <track kind="captions" />
               </video>
